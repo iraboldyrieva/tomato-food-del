3ce16f609a187c480542665b26ba3069bac288de
iraboldyrieva <iraboldyrieva@gmail.com>
2024-09-26 14:07:04 +0300

    Add Order Model and Controller

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -10,6 +10,6 @@ const cartRouter = express.Router();
 
 cartRouter.post("/add", authMiddleware, addToCart);
 cartRouter.post("/remove", authMiddleware, removeFromCart);
-cartRouter.get("/get", authMiddleware, getItems);
+cartRouter.post("/get", authMiddleware, getItems);
 
 export default cartRouter;
